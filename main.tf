a9fad337eb09262297c6ff51eba17082eb0d9839
sivakumaraddala26 <80095151+sivakumaraddala26@users.noreply.github.com>
2025-03-12 23:16:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,136 +1,46 @@
-#This Terraform Code Deploys Basic VPC Infra.
 provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    region = "${var.aws_region}"
+    access_key = "aws access key"
+    secret_key = "aws sec key"
+    region = "us-east-1"
+    #profile = "siva"
 }
 
-resource "aws_vpc" "default" {
-    cidr_block = "${var.vpc_cidr}"
-    enable_dns_hostnames = true
-    tags = {
-        Name = "${var.vpc_name}"
-	Owner = "Sreeharsha Veerapalli"
-	environment = "${var.environment}"
-    }
-}
 
-resource "aws_internet_gateway" "default" {
-    vpc_id = "${aws_vpc.default.id}"
-	tags = {
-        Name = "${var.IGW_name}"
-    }
-}
+resource "aws_instance" "Test_server" {
+    ami = "ami-04b4f1a9cf54c11d0"
+    instance_type = "t2.micro"
+    user_data = <<EOF
+    #!/bin/bash
+    apt update -y
+    apt-get install python3
+    python3 --version
+    apt install httpd -y
+    service httpd start
+    chkconfig httpd on
+    cd /var/www/html
+    echo "<html><h1>Hello Cloud Gurus Welcome To My Webpage</h1></html>" >
+    index.html
+    echo "User Data printed successfully"
+  EOF
 
-resource "aws_subnet" "subnet1-public" {
-    vpc_id = "${aws_vpc.default.id}"
-    cidr_block = "${var.public_subnet1_cidr}"
-    availability_zone = "us-east-1a"
 
     tags = {
-        Name = "${var.public_subnet1_name}"
+        Name = "Test_server"
     }
-}
-
-resource "aws_subnet" "subnet2-public" {
-    vpc_id = "${aws_vpc.default.id}"
-    cidr_block = "${var.public_subnet2_cidr}"
-    availability_zone = "us-east-1b"
-
-    tags = {
-        Name = "${var.public_subnet2_name}"
-    }
-}
-
-resource "aws_subnet" "subnet3-public" {
-    vpc_id = "${aws_vpc.default.id}"
-    cidr_block = "${var.public_subnet3_cidr}"
-    availability_zone = "us-east-1c"
-
-    tags = {
-        Name = "${var.public_subnet3_name}"
-    }
-	
+  
 }
 
 
-resource "aws_route_table" "terraform-public" {
-    vpc_id = "${aws_vpc.default.id}"
-
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.default.id}"
-    }
-
-    tags = {
-        Name = "${var.Main_Routing_Table}"
-    }
+output "instance_details" {
+    value = aws_instance.Test_server.id
+    description = "ID of the aws instance"
 }
 
-resource "aws_route_table_association" "terraform-public" {
-    subnet_id = "${aws_subnet.subnet1-public.id}"
-    route_table_id = "${aws_route_table.terraform-public.id}"
+output "instance_ip" {
+    value = aws_instance.Test_server.public_ip
+    description = "IP address of the aws instance"
 }
-
-resource "aws_security_group" "allow_all" {
-  name        = "allow_all"
-  description = "Allow all inbound traffic"
-  vpc_id      = "${aws_vpc.default.id}"
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-    }
+output "instance_vpc_ip" {
+    value = aws_instance.Test_server.private_ip
+    description = "IP address of the aws instance"
 }
-
-# data "aws_ami" "my_ami" {
-#      most_recent      = true
-#      #name_regex       = "^mavrick"
-#      owners           = ["721834156908"]
-# }
-
-
-# resource "aws_instance" "web-1" {
-#     ami = var.imagename
-#     #ami = "ami-0d857ff0f5fc4e03b"
-#     #ami = "${data.aws_ami.my_ami.id}"
-#     availability_zone = "us-east-1a"
-#     instance_type = "t2.micro"
-#     key_name = "LaptopKey"
-#     subnet_id = "${aws_subnet.subnet1-public.id}"
-#     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
-#     associate_public_ip_address = true	
-#     tags = {
-#         Name = "Server-1"
-#         Env = "Prod"
-#         Owner = "Sree"
-# 	CostCenter = "ABCD"
-#     }
-# }
-
-##output "ami_id" {
-#  value = "${data.aws_ami.my_ami.id}"
-#}
-#!/bin/bash
-# echo "Listing the files in the repo."
-# ls -al
-# echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++"
-# echo "Running Packer Now...!!"
-# packer build -var=aws_access_key=AAAAAAAAAAAAAAAAAA -var=aws_secret_key=BBBBBBBBBBBBB packer.json
-#packer validate --var-file creds.json packer.json
-#packer build --var-file creds.json packer.json
-#packer.exe build --var-file creds.json -var=aws_access_key=AAAAAAAAAAAAAAAAAA -var=aws_secret_key=BBBBBBBBBBBBB packer.json
-# echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++"
-# echo "Running Terraform Now...!!"
-# terraform init
-# terraform apply --var-file terraform.tfvars -var="aws_access_key=AAAAAAAAAAAAAAAAAA" -var="aws_secret_key=BBBBBBBBBBBBB" --auto-approve
-#https://discuss.devopscube.com/t/how-to-get-the-ami-id-after-a-packer-build/36
